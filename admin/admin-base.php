polylang/polylang

commit 3ab0d8dcd9a3e030635d3113b7983bbecd16ffcd

The backend current language equals those of current post or term being edited
Three languages are now defined on backend:
* filter_lang => created by this commit, it is the current status of the
admin languages filter in the admin bar. May be empty when it displays
"Show all languages"
* curlang     => is now equal to the language of the post or term being
edited, equal to filter_lang otherwise. It's the language used to filter
the content (get_posts and get_terms are now filtered by default)
* pref_lang   => preferred language used as default when saving posts or
terms (unchanged)

The commit also moves have_translated_taxonomy() from PLL_Frontend to
PLL_Model to make it reusable.

Fixes #19

admin/admin-base.php
@@ -6,7 +6,7 @@
  * @since 1.8
  */
 class PLL_Admin_Base extends PLL_Base {
-	public $curlang, $pref_lang;
+	public $filter_lang, $curlang, $pref_lang;
 
 	/**
 	 * loads the polylang text domain
@@ -170,6 +170,41 @@ class PLL_Admin_Base extends PLL_Base {
 	}
 
 	/**
+	 * Sets the admin current language, used to filter the content
+	 *
+	 * @since 2.0
+	 */
+	public function set_current_language() {
+		$this->curlang = $this->filter_lang;
+
+		// Edit Post
+		if ( isset( $_REQUEST['pll_post_id'] ) ) {
+			$this->curlang = $this->model->post->get_language( (int) $_REQUEST['pll_post_id'] );
+		} elseif ( 'post.php' === $GLOBALS['pagenow'] && isset( $_GET['post'] ) && is_numeric( $_GET['post'] ) ) {
+			$this->curlang = $this->model->post->get_language( (int) $_GET['post'] );
+		} elseif ( 'post-new.php' === $GLOBALS['pagenow'] ) {
+			$this->curlang = empty( $_GET['new_lang'] ) ? $this->pref_lang : $this->model->get_language( $_GET['new_lang'] );
+		}
+
+		// Edit Term
+		// FIXME 'edit-tags.php' for backward compatibility with WP < 4.5
+		elseif ( in_array( $GLOBALS['pagenow'], array( 'edit-tags.php', 'term.php' ) ) && isset( $_GET['tag_ID'] ) ) {
+			$this->curlang = $this->model->term->get_language( (int) $_GET['tag_ID'] );
+		} elseif ( 'edit-tags.php' === $GLOBALS['pagenow'] ) {
+			if ( ! empty( $_GET['new_lang'] ) ) {
+				$this->curlang = $this->model->get_language( $_GET['new_lang'] );
+			} elseif ( empty( $this->curlang ) ) {
+				$this->curlang = $this->pref_lang;
+			}
+		}
+
+		// Ajax
+		elseif ( ! empty( $_REQUEST['lang'] ) ) {
+			$this->curlang = $this->model->get_language( $_REQUEST['lang'] );
+		}
+	}
+
+	/**
 	 * defines the backend language and the admin language filter based on user preferences
 	 *
 	 * @since 1.2.3
@@ -184,10 +219,10 @@ class PLL_Admin_Base extends PLL_Base {
 			update_user_meta( $user_id, 'pll_filter_content', ( $lang = $this->model->get_language( $_GET['lang'] ) ) ? $lang->slug : '' );
 		}
 
-		$this->curlang = $this->model->get_language( get_user_meta( get_current_user_id(), 'pll_filter_content', true ) );
+		$this->filter_lang = $this->model->get_language( get_user_meta( get_current_user_id(), 'pll_filter_content', true ) );
 
 		// set preferred language for use when saving posts and terms: must not be empty
-		$this->pref_lang = empty( $this->curlang ) ? $this->model->get_language( $this->options['default_lang'] ) : $this->curlang;
+		$this->pref_lang = empty( $this->filter_lang ) ? $this->model->get_language( $this->options['default_lang'] ) : $this->filter_lang;
 
 		/**
 		 * Filter the preferred language on amin side
@@ -198,6 +233,8 @@ class PLL_Admin_Base extends PLL_Base {
 		 * @param object $pref_lang preferred language
 		 */
 		$this->pref_lang = apply_filters( 'pll_admin_preferred_language', $this->pref_lang );
+
+		$this->set_current_language();
 
 		// inform that the admin language has been set
 		// only if the admin language is one of the Polylang defined language
@@ -258,7 +295,7 @@ class PLL_Admin_Base extends PLL_Base {
 			'flag' => '<span class="ab-icon"></span>',
 		);
 
-		$selected = empty( $this->curlang ) ? $all_item : $this->curlang;
+		$selected = empty( $this->filter_lang ) ? $all_item : $this->filter_lang;
 
 		$wp_admin_bar->add_menu( array(
 			'id'     => 'languages',

admin/admin-filters-columns.php
@@ -7,7 +7,7 @@
  * @since 1.2
  */
 class PLL_Admin_Filters_Columns {
-	public $links, $model, $curlang;
+	public $links, $model, $filter_lang;
 
 	/**
 	 * constructor: setups filters and actions
@@ -19,7 +19,7 @@ class PLL_Admin_Filters_Columns {
 	public function __construct( &$polylang ) {
 		$this->links = &$polylang->links;
 		$this->model = &$polylang->model;
-		$this->curlang = &$polylang->curlang;
+		$this->filter_lang = &$polylang->filter_lang;
 
 		// add the language and translations columns in 'All Posts', 'All Pages' and 'Media library' panels
 		foreach ( $this->model->get_translated_post_types() as $type ) {
@@ -61,7 +61,7 @@ class PLL_Admin_Filters_Columns {
 
 		foreach ( $this->model->get_languages_list() as $language ) {
 			// don't add the column for the filtered language
-			if ( empty( $this->curlang ) || $language->slug != $this->curlang->slug ) {
+			if ( empty( $this->filter_lang ) || $language->slug != $this->filter_lang->slug ) {
 				$columns[ 'language_'.$language->slug ] = $language->flag ? $language->flag . '<span class="screen-reader-text">' . esc_html( $language->name ) . '</span>' : esc_html( $language->slug );
 			}
 		}
@@ -78,7 +78,7 @@ class PLL_Admin_Filters_Columns {
 	 */
 	protected function get_first_language_column() {
 		foreach ( $this->model->get_languages_list() as $language ) {
-			if ( empty( $this->curlang ) || $language->slug != $this->curlang->slug ) {
+			if ( empty( $this->filter_lang ) || $language->slug != $this->filter_lang->slug ) {
 				$columns[] = 'language_' . $language->slug;
 			}
 		}

admin/admin-filters-post.php
@@ -113,14 +113,40 @@ class PLL_Admin_Filters_Post extends PLL_Admin_Filters_Post_Base {
 			return;
 		}
 
-		if ( isset( $qvars['post_type'] ) && ! isset( $qvars['lang'] ) ) {
-			// filters the list of media ( or wp-links ) by language when uploading from post
-			if ( isset( $_REQUEST['pll_post_id'] ) && $lang = $this->model->post->get_language( (int) $_REQUEST['pll_post_id'] ) ) {
-				$query->set( 'lang', $lang->slug );
-			} elseif ( ! empty( $this->curlang ) ) {
+		// Do not filter the query if the language is already specified in another way
+		if ( ! isset( $qvars['lang'] ) ) {
+			$excludes = array(
+				'p', 'post_parent', 'attachment', 'attachment_id', 'name', 'pagename', 'page_id',
+				'category_name', 'tag', 'cat', 'tag_id', 'category__in', 'category__not_in', 'category__and',
+				'post__in', 'post__not_in', 'post_name__in', 'tag__in', 'tag__not_in', 'tag__and',
+				'tag_slug__in', 'tag_slug__and', 'post_parent__in', 'post_parent__not_in',
+			);
+
+			foreach ( $excludes as $k ) {
+				if ( ! empty( $qvars[ $k ] ) ) {
+					return;
+				}
+			}
+
+			$taxonomies = array_intersect( $this->model->get_translated_taxonomies(), get_taxonomies( array( '_builtin' => false ) ) );
+
+			foreach( $taxonomies as $tax ) {
+				$tax = get_taxonomy( $tax );
+				if ( ! empty( $qv[ $tax->query_var ] ) ) {
+					return;
+				}
+			}
+
+			if ( ! empty( $qvars['tax_query'] ) && is_array( $qvars['tax_query'] ) && $this->model->have_translated_taxonomy( $qvars['tax_query'] ) ) {
+				return;
+			}
+
+			// Filter queries according to the current language
+			if ( isset( $qvars['post_type'] ) && ! empty( $this->curlang ) ) {
 				$qvars['lang'] = $this->curlang->slug;
 			}
 		}
+
 
 		if ( isset( $qvars['lang'] ) && 'all' === $qvars['lang'] ) {
 			unset( $qvars['lang'] );

admin/admin-filters-term.php
@@ -6,7 +6,7 @@
  * @since 1.2
  */
 class PLL_Admin_Filters_Term {
-	public $links, $model, $options, $curlang, $pref_lang;
+	public $links, $model, $options, $curlang, $filter_lang, $pref_lang;
 	protected $pre_term_name; // Used to store the term name before creating a slug if needed
 	protected $post_id; // Used to store the current post_id when bulk editing posts
 
@@ -20,6 +20,7 @@ class PLL_Admin_Filters_Term {
 		$this->model = &$polylang->model;
 		$this->options = &$polylang->options;
 		$this->curlang = &$polylang->curlang;
+		$this->filter_lang = &$polylang->filter_lang;
 		$this->pref_lang = &$polylang->pref_lang;
 
 		foreach ( $this->model->get_translated_taxonomies() as $tax ) {
@@ -575,88 +576,12 @@ class PLL_Admin_Filters_Term {
 			return $args['lang'];
 		}
 
-		if ( function_exists( 'get_current_screen' ) ) {
-			$screen = get_current_screen(); // since WP 3.1, may not be available the first time(s) get_terms is called
+		// On tags page, the tags list and the tag cloud must be filtered according to the admin language filter
+		if ( 'edit-tags.php' === $GLOBALS['pagenow'] && ( ! empty( $args['page'] ) || ! empty( $args['smallest'] ) ) ) {
+			return $this->filter_lang;
 		}
 
-		// Don't filter nav menus on nav menus screen
-		if ( isset( $screen ) && 'nav-menus' == $screen->base && in_array( 'nav_menu', $taxonomies ) ) {
-			return false;
-		}
-
-		// Does nothing in Languages and dasboard admin panels
-		if ( isset( $screen ) && in_array( $screen->base, array( 'toplevel_page_mlang', 'dashboard' ) ) ) {
-			return false;
-		}
-
-		// Ajax actions
-		if ( isset( $_POST['action'] ) ) {
-			// Admin language filter for:
-			// Ajax paginate_links in taxonomies metabox in nav menus panel
-			// And taxonomies menus items in customizer menus ( since WP 4.3 )
-			if ( ! empty( $this->curlang ) && in_array( $_POST['action'], array( 'menu-get-metabox', 'load-available-menu-items-customizer' ) ) ) {
-				return $this->curlang;
-			}
-
-			// The only ajax response I want to deal with is when changing the language in post metabox
-			if ( ! in_array( $_POST['action'], array( 'post_lang_choice', 'term_lang_choice', 'get-tagcloud' ) ) ) {
-				return false;
-			}
-
-			// I only want to filter the parent dropdown list when editing a term in a hierarchical taxonomy
-			if ( 'term_lang_choice' == $_POST['action'] && ! ( isset( $args['class'] ) || isset( $args['unit'] ) ) ) {
-				return false;
-			}
-		}
-
-		// Ajax response for changing the language in the post metabox ( or in the edit-tags panels )
-		if ( isset( $_POST['lang'] ) ) {
-			$lang = $this->model->get_language( $_POST['lang'] );
-		}
-
-		// The post ( or term ) is created with the 'add new' ( translation ) link
-		// Test of $args['page'] to avoid filtering the terms list table in edit-tags panel
-		elseif ( ! empty( $_GET['new_lang'] ) && empty( $args['page'] ) ) {
-			$lang = $this->model->get_language( $_GET['new_lang'] );
-		}
-
-		// FIXME can we simplify how we deal with the admin language filter?
-		// The language filter selection has just changed
-		// Test $screen->base to avoid interference between the language filter and the post language selection and the category parent dropdown list
-		elseif ( ! empty( $_GET['lang'] ) && ! ( isset( $screen ) && in_array( $screen->base, array( 'post', 'edit-tags', 'term' ) ) ) ) {
-			if ( 'all' != $_GET['lang'] ) {
-				$lang = $this->model->get_language( $_GET['lang'] );
-			}
-			elseif ( in_array( $screen->base, array( 'edit-tags', 'term' ) ) && isset( $args['class'] ) ) {
-				$lang = $this->pref_lang; // Parent dropdown
-			}
-		}
-
-		// Again the language filter
-		elseif ( ! empty( $this->curlang ) && ( isset( $screen ) && 'post' != $screen->base && ! ( in_array( $screen->base, array( 'edit-tags', 'term' ) ) && isset( $args['class'] ) ) ) ) { // don't apply to post edit and the category parent dropdown list
-		 	$lang = $this->curlang;
-		}
-
-		elseif ( isset( $_GET['post'] ) && is_numeric( $_GET['post'] ) ) { // is numeric avoids array of posts in *post* bulk edit
-			$lang = $this->model->post->get_language( $_GET['post'] );
-		}
-
-		// For the parent dropdown list in edit term
-		elseif ( isset( $_GET['tag_ID'] ) ) {
-			$lang = $this->model->term->get_language( (int) $_GET['tag_ID'] );
-		}
-
-		// When a new category is created in the edit post panel
-		elseif ( isset( $_POST['term_lang_choice'] ) ) {
-			$lang = $this->model->get_language( $_POST['term_lang_choice'] );
-		}
-
-		// For a new post ( or the parent dropdown list of a new term )
-		elseif ( isset( $screen ) && ( 'post' == $screen->base || ( in_array( $screen->base, array( 'edit-tags', 'term' ) ) && isset( $args['class'] ) ) ) ) {
-			$lang = $this->pref_lang;
-		}
-
-		return empty( $lang ) ? false : $lang;
+		return $this->curlang;
 	}
 
 	/**

admin/admin.php
@@ -11,7 +11,8 @@
  * links            => inherited, reference to PLL_Admin_Links object
  * static_pages     => inherited, reference to PLL_Admin_Static_Pages object
  * filters_links    => inherited, reference to PLL_Filters_Links object
- * curlang          => inherited, optional, current language used to filter admin content
+ * curlang          => inherited, optional, current language used to filter the content (language of the post or term being edited, equal to filter_lang otherwise)
+ * filter_lang      => inherited, optional, current status of the admin languages filter (in the admin bar)
  * pref_lang        => inherited, preferred language used as default when saving posts or terms
  * filters          => reference to PLL_Filters object
  * filters_columns  => reference to PLL_Admin_Filters_Columns object

frontend/frontend.php
@@ -123,7 +123,7 @@ class PLL_Frontend extends PLL_Base {
 		$qv = $query->query_vars;
 
 		// to avoid returning an empty result if the query includes a translated taxonomy in a different language
-		$has_tax = isset( $query->tax_query->queries ) && $this->have_translated_taxonomy( $query->tax_query->queries );
+		$has_tax = isset( $query->tax_query->queries ) && $this->model->have_translated_taxonomy( $query->tax_query->queries );
 
 		// allow filtering recent posts and secondary queries by the current language
 		// take care not to break queries for non visible post types such as nav_menu_items
@@ -198,31 +198,6 @@ class PLL_Frontend extends PLL_Base {
 			$this->static_pages->init();
 			$this->load_strings_translations();
 		}
-	}
-
-	/**
-	 * check if translated taxonomy is queried
-	 * compatible with nested queries introduced in WP 4.1
-	 * @see https://wordpress.org/support/topic/tax_query-bug
-	 *
-	 * @since 1.7
-	 *
-	 * @param array $tax_queries
-	 * @return bool
-	 */
-	protected function have_translated_taxonomy( $tax_queries ) {
-		foreach ( $tax_queries as $tax_query ) {
-			if ( isset( $tax_query['taxonomy'] ) && $this->model->is_translated_taxonomy( $tax_query['taxonomy'] ) && ! ( isset( $tax_query['operator'] ) && 'NOT IN' === $tax_query['operator'] ) ) {
-				return true;
-			}
-
-			// nested queries
-			elseif ( is_array( $tax_query ) && $this->have_translated_taxonomy( $tax_query ) ) {
-				return true;
-			}
-		}
-
-		return false;
 	}
 
 	/**

include/model.php
@@ -181,7 +181,7 @@ class PLL_Model {
 	 * @return array modifed list of clauses
 	 */
 	public function terms_clauses( $clauses, $lang ) {
-		if ( ! empty( $lang ) ) {
+		if ( ! empty( $lang ) && false === strpos( $clauses['join'], 'pll_tr' ) ) {
 			$clauses['join'] .= $this->term->join_clause();
 			$clauses['where'] .= $this->term->where_clause( $lang );
 		}
@@ -291,6 +291,31 @@ class PLL_Model {
 	public function is_translated_taxonomy( $tax ) {
 		$taxonomies = $this->get_translated_taxonomies( false );
 		return ( is_array( $tax ) && array_intersect( $tax, $taxonomies ) || in_array( $tax, $taxonomies ) );
+	}
+
+	/**
+	 * Check if translated taxonomy is queried
+	 * Compatible with nested queries introduced in WP 4.1
+	 * @see https://wordpress.org/support/topic/tax_query-bug
+	 *
+	 * @since 1.7
+	 *
+	 * @param array $tax_queries
+	 * @return bool
+	 */
+	public function have_translated_taxonomy( $tax_queries ) {
+		foreach ( $tax_queries as $tax_query ) {
+			if ( isset( $tax_query['taxonomy'] ) && $this->is_translated_taxonomy( $tax_query['taxonomy'] ) && ! ( isset( $tax_query['operator'] ) && 'NOT IN' === $tax_query['operator'] ) ) {
+				return true;
+			}
+
+			// Nested queries
+			elseif ( is_array( $tax_query ) && $this->have_translated_taxonomy( $tax_query ) ) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	/**

RED0731/red_polylang

commit 22409d692429f7a168cea57710f20a78130808cb

Fix categories metabox not updated when changing the language
Bug introduced by the commit 3ab0d8dcd9a3e030635d3113b7983bbecd16ffcd

admin/admin-base.php
@@ -192,7 +192,7 @@ class PLL_Admin_Base extends PLL_Base {
 		}
 
 		// Ajax
-		elseif ( ! empty( $_REQUEST['lang'] ) ) {
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX && ! empty( $_REQUEST['lang'] ) ) {
 			$this->curlang = $this->model->get_language( $_REQUEST['lang'] );
 		}
 	}