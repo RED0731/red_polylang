polylang/polylang

commit 19784c8ca4b27bcc474d22942fd1317b7512db29

Load static page class only if we are using a static front page

frontend/frontend.php
@@ -55,7 +55,11 @@ class PLL_Frontend extends PLL_Base {
 	 */
 	public function init() {
 		$this->links = new PLL_Frontend_Links( $this );
-		$this->static_pages = new PLL_Frontend_Static_Pages( $this );
+
+		// Static front page and page for posts
+		if ( 'page' === get_option( 'show_on_front' ) ) {
+			$this->static_pages = new PLL_Frontend_Static_Pages( $this );
+		}
 
 		// setup the language chooser
 		$c = array( 'Content', 'Url', 'Url', 'Domain' );

RED0731/red_polylang

commit 9650f0646130fae5f020b6d81c0a22655b6d06e1

Fix fatal error on multisite when the front page displays posts
Introduced by 19784c8ca4b27bcc474d22942fd1317b7512db29

frontend/frontend.php
@@ -169,7 +169,11 @@ class PLL_Frontend extends PLL_Base {
 
 			$lang = $this->model->get_language( $restore_curlang );
 			$this->curlang = $lang ? $lang : $this->model->get_language( $this->options['default_lang'] );
-			$this->static_pages->init();
+
+			if ( isset( $this->static_pages ) ) {
+				$this->static_pages->init();
+			}
+
 			$this->load_strings_translations();
 		}
 	}