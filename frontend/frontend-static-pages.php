polylang/polylang

commit 56fa5c3b9dc89d3143621c55ec537120cf548186

Fix home page not correctly loaded when adding a query var
Fix https://github.com/polylang/polylang-pro/issues/85

frontend/frontend-static-pages.php
@@ -178,8 +178,7 @@ class PLL_Frontend_Static_Pages extends PLL_Static_Pages {
 		}
 
 		// Redirect the language page to the homepage when using a static front page
-		elseif ( ( $this->options['redirect_lang'] || $this->options['hide_default'] ) && ( count( $query->query ) == 1 || ( ( is_preview() || is_paged() || ! empty( $query->query['page'] ) ) && count( $query->query ) == 2 ) || ( ( is_preview() && ( is_paged() || ! empty( $query->query['page'] ) ) ) && count( $query->query ) == 3 ) ) && is_tax( 'language' ) ) {
-			$lang = $this->model->get_language( get_query_var( 'lang' ) );
+		elseif ( ( $this->options['redirect_lang'] || $this->options['hide_default'] ) && is_tax() && 1 === count( $query->tax_query->queries ) && $lang = $this->model->get_language( get_query_var( 'lang' ) ) ) {
 			$query->set( 'page_id', $lang->page_on_front );
 			$query->is_singular = $query->is_page = true;
 			$query->is_archive = $query->is_tax = false;

RED0731/red_polylang

commit a94bba6bfbba11ddf7afc46755d517b8c1682c68

Fix archives when redirecting the language page to the homepage
Bug introduced in 56fa5c3b9dc89d3143621c55ec537120cf548186
Fix https://github.com/polylang/polylang-pro/issues/89

frontend/frontend-static-pages.php
@@ -159,6 +159,18 @@ class PLL_Frontend_Static_Pages extends PLL_Static_Pages {
 	}
 
 	/**
+	 * Is the query for a the static front page (redirected from the language page)?
+	 *
+	 * @since 2.3
+	 *
+	 * @param object $query
+	 * @return bool
+	 */
+	protected function is_front_page( $query ) {
+		return ! is_date() && ! is_author() && ! is_search() && ! is_feed() && ! is_post_type_archive() && is_tax() && 1 === count( $query->tax_query->queries );
+	}
+
+	/**
 	 * Setups query vars when requesting a static front page
 	 *
 	 * @since 1.8
@@ -178,7 +190,7 @@ class PLL_Frontend_Static_Pages extends PLL_Static_Pages {
 		}
 
 		// Redirect the language page to the homepage when using a static front page
-		elseif ( ( $this->options['redirect_lang'] || $this->options['hide_default'] ) && is_tax() && 1 === count( $query->tax_query->queries ) && $lang = $this->model->get_language( get_query_var( 'lang' ) ) ) {
+		elseif ( ( $this->options['redirect_lang'] || $this->options['hide_default'] ) && $this->is_front_page( $query ) && $lang = $this->model->get_language( get_query_var( 'lang' ) ) ) {
 			$query->set( 'page_id', $lang->page_on_front );
 			$query->is_singular = $query->is_page = true;
 			$query->is_archive = $query->is_tax = false;