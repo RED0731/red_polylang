polylang/polylang

commit 56fa5c3b9dc89d3143621c55ec537120cf548186

Fix home page not correctly loaded when adding a query var
Fix https://github.com/polylang/polylang-pro/issues/85

frontend/frontend-static-pages.php
@@ -178,8 +178,7 @@ class PLL_Frontend_Static_Pages extends PLL_Static_Pages {
 		}
 
 		// Redirect the language page to the homepage when using a static front page
-		elseif ( ( $this->options['redirect_lang'] || $this->options['hide_default'] ) && ( count( $query->query ) == 1 || ( ( is_preview() || is_paged() || ! empty( $query->query['page'] ) ) && count( $query->query ) == 2 ) || ( ( is_preview() && ( is_paged() || ! empty( $query->query['page'] ) ) ) && count( $query->query ) == 3 ) ) && is_tax( 'language' ) ) {
-			$lang = $this->model->get_language( get_query_var( 'lang' ) );
+		elseif ( ( $this->options['redirect_lang'] || $this->options['hide_default'] ) && is_tax() && 1 === count( $query->tax_query->queries ) && $lang = $this->model->get_language( get_query_var( 'lang' ) ) ) {
 			$query->set( 'page_id', $lang->page_on_front );
 			$query->is_singular = $query->is_page = true;
 			$query->is_archive = $query->is_tax = false;

RED0731/red_polylang

commit 603592ef550543568ea7c6fc98c075a210aa84a4

Fix custom query var redirected to home page
Bug introduced in 56fa5c3b9dc89d3143621c55ec537120cf548186
Fix #216

frontend/frontend-static-pages.php
@@ -167,7 +167,8 @@ class PLL_Frontend_Static_Pages extends PLL_Static_Pages {
 	 * @return bool
 	 */
 	protected function is_front_page( $query ) {
-		return ! is_date() && ! is_author() && ! is_search() && ! is_feed() && ! is_post_type_archive() && is_tax() && 1 === count( $query->tax_query->queries );
+		$query = array_diff( array_keys( $query->query ), array( 'preview', 'page', 'paged', 'cpage', 'orderby' ) );
+		return 1 === count( $query ) && in_array( 'lang', $query );
 	}
 
 	/**

tests/phpunit/tests/test-static-pages.php
@@ -482,7 +482,7 @@ class Static_Pages_Test extends PLL_UnitTestCase {
 		$this->assertEquals( array( get_post( $en ) ), $GLOBALS['wp_query']->posts );
 	}
 
-		// Bug introduced and fixed in 2.3
+	// Bug introduced and fixed in 2.3
 	function test_post_type_archives_with_front_page_with_redirect_lang() {
 		global $wp_rewrite;
 
@@ -507,5 +507,58 @@ class Static_Pages_Test extends PLL_UnitTestCase {
 		$this->assertEquals( array( get_post( $en ) ), $GLOBALS['wp_query']->posts );
 
 		_unregister_post_type( 'trcpt' );
+	}
+
+	// Add custom query var
+	function extra_query_vars( $query_vars ) {
+		$query_vars[] = 'action';
+		return $query_vars;
+	}
+
+	// Add custom root rewrite rule
+	function extra_root_rewrite_rules( $rules ) {
+		$rules['^testing/?$'] = 'index.php?action=testing';
+		return $rules;
+	}
+
+	// Bug introduced in 2.3 and fixed in 2.3.1
+	function test_extra_query_var_with_front_page_with_query_with_redirect_lang() {
+		global $wp_rewrite;
+
+		add_filter( 'query_vars', array( $this, 'extra_query_vars' ) );
+		add_filter( 'root_rewrite_rules', array( $this, 'extra_root_rewrite_rules' ), 1 );
+
+		self::$polylang->options['hide_default'] = 1;
+		self::$polylang->options['redirect_lang'] = 1;
+		self::$polylang->model->clean_languages_cache();
+
+		$wp_rewrite->init();
+		$wp_rewrite->extra_rules_top = array(); // brute force since WP does not do it :(
+		$wp_rewrite->flush_rules();
+
+		self::$polylang->curlang = self::$polylang->model->get_language( 'en' ); // brute force
+		$this->go_to( home_url( '/testing/' ) );
+
+		$this->assertFalse( is_front_page() );
+	}
+
+	function test_front_page_with_orderby_with_redirect_lang() {
+		global $wp_rewrite;
+
+		self::$polylang->options['redirect_lang'] = 1;
+		self::$polylang->model->clean_languages_cache();
+
+		$wp_rewrite->init();
+		$wp_rewrite->extra_rules_top = array(); // brute force since WP does not do it :(
+		$wp_rewrite->flush_rules();
+
+		self::$polylang->curlang = self::$polylang->model->get_language( 'fr' ); // brute force
+		$this->go_to( home_url( '/fr/?orderby=price' ) );
+
+		$this->assertTrue( is_front_page() );
+		$this->assertQueryTrue( 'is_page', 'is_singular', 'is_front_page' );
+		$this->assertEquals( home_url( '/en/' ), self::$polylang->links->get_translation_url( self::$polylang->model->get_language( 'en' ) ) );
+		$this->assertEquals( array( get_post( self::$home_fr ) ), $GLOBALS['wp_query']->posts );
+		$this->assertEmpty( redirect_canonical( home_url( '/fr/?orderby=price' ), false ) );
 	}
 }