26522a9e931a2b8debc4c55eb25469ed2884ab86
RED0731 <tomb24708@gmail.com>
2021-01-27 18:07:34 +0100

    POC to add the languages field on new legacy widget

--- include/filters.php
+++ include/filters.php
@@ -82,6 +82,10 @@ class PLL_Filters {
 
 		// Personal data exporter
 		add_filter( 'wp_privacy_personal_data_exporters', array( $this, 'register_personal_data_exporter' ), 0 ); // Since WP 4.9.6
+
+		// Widgets languages filter
+		add_action( 'in_widget_form', array( $this, 'in_widget_form' ), 10, 3 );
+		add_filter( 'widget_update_callback', array( $this, 'widget_update_callback' ), 10, 4 );
 	}
 
 	/**
@@ -409,5 +413,64 @@ class PLL_Filters {
 			'data' => $data_to_export,
 			'done' => true,
 		);
+	}
+
+	/**
+	 * Modifies the widgets forms to add our language dropdown list.
+	 *
+	 * @since 0.3
+	 *
+	 * @param WP_Widget $widget   Widget instance.
+	 * @param null      $return   Not used.
+	 * @param array     $instance Widget settings.
+	 * @return void
+	 */
+	public function in_widget_form( $widget, $return, $instance ) {
+
+		$dropdown = new PLL_Walker_Dropdown();
+
+		$dropdown_html = $dropdown->walk(
+			array_merge(
+				array( (object) array( 'slug' => 0, 'name' => __( 'All languages', 'polylang' ) ) ),
+				$this->model->get_languages_list()
+			),
+			-1,
+			array(
+				'name'     => $widget->id . '_lang_choice',
+				'class'    => 'tags-input pll-lang-choice',
+				'selected' => empty( $instance['pll_lang'] ) ? '' : $instance['pll_lang'],
+			)
+		);
+
+		printf(
+			'<p><label for="%1$s">%2$s %3$s</label></p>',
+			esc_attr( $widget->id . '_lang_choice' ),
+			esc_html__( 'The widget is displayed for:', 'polylang' ),
+			$dropdown_html // phpcs:ignore WordPress.Security.EscapeOutput
+		);
+	}
+
+	/**
+	 * Called when widget options are saved.
+	 * Saves the language associated to the widget.
+	 *
+	 * @since 0.3
+	 *
+	 * @param array     $instance     Widget options.
+	 * @param array     $new_instance Not used.
+	 * @param array     $old_instance Not used.
+	 * @param WP_Widget $widget       WP_Widget object.
+	 * @return array Widget options.
+	 */
+	public function widget_update_callback( $instance, $new_instance, $old_instance, $widget ) {
+		$key = $widget->id . '_lang_choice';
+
+		if ( ! empty( $_POST[ $key ] ) && $lang = $this->model->get_language( sanitize_key( $_POST[ $key ] ) ) ) { // phpcs:ignore WordPress.Security.NonceVerification
+			$instance['pll_lang'] = $lang->slug;
+		} else {
+			unset( $instance['pll_lang'] );
+		}
+
+		return $instance;
 	}
 }
